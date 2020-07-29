c311d587a3092563e37ca6b3ad66e8405aa91a37
wrp1002 <wes.hamster@gmail.com>
2020-07-29 04:38:49 -0400

    icon

--- update.sh
+++ update.sh
@@ -39,6 +39,7 @@ rm Packages.bz2
   echo SHA1: $(sha1sum "$fn" | cut -d" " -f1)
   echo SHA256: $(sha256sum "$fn" | cut -d" " -f1)
   echo Depiction: https://wrp1002.github.io/depiction/"$fn"/index.html | sed "s/com.wrp1002.//g" | sed "s/.deb//g" | sed 's;_.*\/;/;g'
+  echo Icon: https://wrp1002.github.io/depiction/"$fn"/icon.png | sed "s/com.wrp1002.//g" | sed "s/.deb//g" | sed 's;_.*\/;/;g'
   echo
 done } | tee Packages
 bzip2 -f Packages
